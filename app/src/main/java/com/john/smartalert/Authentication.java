b5b922827a63b52c8328a322b4bc0944be6059c5
John Vlamis <123981368+johnvl02@users.noreply.github.com>
2024-02-20 14:03:33 +0200

    change the display name

--- app/src/main/java/com/john/smartalert/Authentication.java
+++ app/src/main/java/com/john/smartalert/Authentication.java
@@ -1,21 +1,13 @@
 package com.john.smartalert;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -25,7 +17,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.john.smartalert.R;
 
 public class Authentication extends AppCompatActivity {
     EditText email,password,fullname;
@@ -91,7 +82,7 @@ public class Authentication extends AppCompatActivity {
                         if(email.getText().toString().contains("@civilprotection.gr"))
                         {
                             Intent intent = new Intent(Authentication.this, EmployeeHomePage.class);
-                            intent.putExtra("fullname",fullname.getText().toString());
+                            intent.putExtra("fullname",auth.getCurrentUser().getDisplayName());
                             intent.putExtra("authId",auth.getUid().toString());
                             //intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                             startActivity(intent);
@@ -99,7 +90,7 @@ public class Authentication extends AppCompatActivity {
                         else
                         {
                             Intent intent = new Intent(Authentication.this, UserHomePage.class);
-                            intent.putExtra("fullname",fullname.getText().toString());
+                            intent.putExtra("fullname",auth.getCurrentUser().getDisplayName());
                             intent.putExtra("authId",auth.getUid().toString());
                             //intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                             startActivity(intent);
